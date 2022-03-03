7a533a15283ca6de8711972457e78128bdf9d737
Callum Pease <cpease@engineyard.com>
2022-03-03 17:59:13 +0000

    [FBZ-10240] Migrate phase-1 of post_bootstrap and custom

---
+++ cookbooks/ey-base/recipes/custom.rb
@@ -0,0 +1,15 @@
+if fetch_env_var(node, "EY_REDIS_ENABLED") =~ /^TRUE$/i
+  include_recipe "redis"
+end
+
+if fetch_env_var(node, "EY_MEMCACHED_ENABLED") =~ /^TRUE$/i
+  include_recipe "memcached"
+end
+
+if fetch_env_var(node, "EY_SIDEKIQ_ENABLED") =~ /^TRUE$/i
+  include_recipe "sidekiq"
+end
+
+if fetch_env_var(node, "EY_LETSENCRYPT_ENABLED") =~ /^TRUE$/i
+  include_recipe "letsencrypt"
+end
