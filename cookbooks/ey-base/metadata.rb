85ac073ef7b2ebe69e6b2518c979d5634e7800b9
Callum Pease <cpease@engineyard.com>
2022-03-04 00:51:00 +0000

    [FBZ-10243] Migrate empheraldisk recipe

--- cookbooks/ey-base/metadata.rb
+++ cookbooks/ey-base/metadata.rb
@@ -2,3 +2,4 @@ name "ey-base"
 version "1.0.0"
 
 depends "ey-ec2"
+depends "ey-ephemeraldisk"
---
+++ cookbooks/ey-ephemeraldisk/recipes/default.rb
@@ -0,0 +1,11 @@
+list_ephemeral_devices().each do |device_info|
+  device = device_info["device"]
+  if device_mounted?(device)
+    Chef::Log.info("#{device} already mounted")
+  elsif File.exist?(device) && device_size_in_bytes(device) > 30000000000
+    mountpoint = get_mountpoint_for_ephemeral_device(device_info)
+    create_fs_and_mount(device, mountpoint)
+  else
+    Chef::Log.info("#{device} does not exist")
+  end
+end
