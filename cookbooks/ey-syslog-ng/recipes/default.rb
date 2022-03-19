8a4a2372609a86f13d10106eb5a9ea61c886a434
Callum Pease <cpease@engineyard.com>
2022-03-19 20:54:58 +0000

    [FBZ10273] Migrate syslog-ng recipe

---
+++ cookbooks/ey-syslog-ng/recipes/default.rb
@@ -0,0 +1,9 @@
+service "syslog-ng" do
+  provider Chef::Provider::Service::Systemd
+  action :nothing
+end
+
+package "syslog-ng" do
+  action :install
+  notifies :restart, "service[syslog-ng]"
+end
