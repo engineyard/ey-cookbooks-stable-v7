796bf9afdd35d0b749ebef46e35ab32102d47551
Kevin <kevvbautista@gmail.com>
2022-06-20 08:05:07 +0800

    Modify Unicorn Recipe to use systemd

--- cookbooks/ey-unicorn/metadata.rb
+++ cookbooks/ey-unicorn/metadata.rb
@@ -1,5 +1,5 @@
 name "ey-unicorn"
 maintainer "Engine Yard"
-version "1.0.0"
+version "1.0.1"
 
 depends "ey-lib"
--- cookbooks/ey-unicorn/recipes/monit_monitoring.rb
+++ cookbooks/ey-unicorn/recipes/monit_monitoring.rb
@@ -55,20 +55,14 @@ node.engineyard.apps.each do |app|
 
   term_conds = app_server_get_worker_termination_conditions(app)
 
-  managed_template "/etc/monit.d/unicorn_#{app.name}.monitrc" do
+  managed_template "/lib/systemd/system/unicorn_#{app.name}.service" do
     owner node.engineyard.environment.ssh_username
     group node.engineyard.environment.ssh_username
     mode "600"
-    source "unicorn.monitrc.erb"
+    source "unicorn.service.erb"
     variables({
       app: app.name,
-      user: node.engineyard.environment.ssh_username,
-      app_type: app.app_type,
-      unicorn_worker_count: [recipe.get_pool_size / node["dna"]["applications"].size, 1].max,
-      environment: node["dna"]["environment"]["framework_env"],
-      master_memory_size: term_conds[:memory_size],
-      master_cycle_count: term_conds[:base_cycles],
-      worker_mem_cycle_checks: term_conds[:memory_cycle_checks],
+      environment: node["dna"]["environment"]["framework_env"]
     })
     backup 0
 
