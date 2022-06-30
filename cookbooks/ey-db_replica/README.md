7611b463d08bd1c917771cf6d23ea3fdeb01cab8
Kevin <kevvbautista@gmail.com>
2022-06-30 11:51:02 +0800

    Migrate DB Slave Recipe to v7

---
+++ cookbooks/ey-db_replica/README.md
@@ -0,0 +1,4 @@
+ey-db_replica
+========
+
+This sets up database replica instances for the Engine Yard PaaS.
---
+++ cookbooks/ey-db_replica/metadata.rb
@@ -0,0 +1,7 @@
+name "ey-db_replica"
+maintainer "Engine Yard"
+
+depends "ey-lib"
+depends "ey-mysql"
+depends "ey-postgresql"
+depends "ey-db_admin_tools"
