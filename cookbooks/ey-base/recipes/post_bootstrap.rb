5c23387165a3306e10ec1b0bc80f026135d93a2c
mushyy <cpease1997@hotmail.com>
2022-07-26 14:18:54 +0100

    [WIP] Migrate reboot recipe, fixes instances on reboots / crashes

--- cookbooks/ey-base/recipes/post_bootstrap.rb
+++ cookbooks/ey-base/recipes/post_bootstrap.rb
@@ -2,6 +2,7 @@ include_recipe "ey-monit"
 include_recipe "ey-collectd"
 include_recipe "ey-nodejs::common"
 include_recipe "ey-nodejs::yarn"
+include_recipe "ey-reboot"
 
 file "/etc/engineyard/recipe-revision.txt" do
   action :touch
