052a2819b5c5a4078d0685972e795eebeb8b3a91
Rupika <rupika.k@developmentlogics.com>
2022-08-23 10:41:06 +0530

    migrate elasticserch cookbook to stack v7

---
+++ cookbooks/ey-elasticsearch/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: elasticsearch
+# Recipe:: default
+#
+# Credit goes to GoTime for their original recipe ( http://cookbooks.opscode.com/cookbooks/elasticsearch )
+
+ES = node['elasticsearch']
+
+include_recipe 'ey-elasticsearch::apt'
+include_recipe 'ey-elasticsearch::install'
+if ES['is_elasticsearch_instance']
+  include_recipe 'ey-elasticsearch::configure_cluster'
+  include_recipe 'ey-elasticsearch::start'
+end
