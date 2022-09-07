59b3256c0b246608ff51062ae45d4ece9f561ebd
Rupika Kapoor <rupikakapoor@users.noreply.github.com>
2022-09-07 22:12:04 +0530

    Update mysql_slave.rb
    fixed indentation

--- cookbooks/ey-mysql/resources/mysql_slave.rb
+++ cookbooks/ey-mysql/resources/mysql_slave.rb
@@ -108,9 +108,9 @@ action :action_mysql_slave do
 
   # the master writes it's uuid to <datadir>/auto.cnf, the slave needs that removed so it will gen it's own
   file "#{node['mysql']['datadir']}/auto.cnf" do
-     action :delete
-     Chef::Log.info("removing <datadir>/auto.cnf")
-     only_if { node["mysql"]["short_version"] >= "5.6" }   
+    action :delete
+    Chef::Log.info("removing <datadir>/auto.cnf")
+    only_if { node["mysql"]["short_version"] >= "5.6" }
   end
 
   include_recipe "ey-mysql::startup"
