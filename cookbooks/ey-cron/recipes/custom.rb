7cc566d837cadc71518eaddda9771f2de1af8222
Kevin <kevvbautista@gmail.com>
2022-10-26 09:15:02 +0800

    Fix Custom Cron for DB and Util instances

--- cookbooks/ey-cron/recipes/custom.rb
+++ cookbooks/ey-cron/recipes/custom.rb
@@ -22,65 +22,65 @@ def delete_cron_jobs_not_in_custom(user, crons)
   end
 end
 
-if crontab_instance?(node)
-  if node["custom_crons"]
-    # Find all cron jobs specified in attributes/cron.rb where current node matches instance_name
-    named_crons = node["custom_crons"].find_all { |c| c[:instance_name] == node["dna"]["name"] }
+if node["custom_crons"]
+  # Find all cron jobs specified in attributes/cron.rb where current node matches instance_name
+  named_crons = node["custom_crons"].find_all { |c| c["instance_name"] == node["dna"]["name"] }
 
-    # Find all cron jobs for utility instances
-    util_crons = node["custom_crons"].find_all { |c| c[:instance_name] == "util" }
+  # Find all cron jobs for utility instances
+  util_crons = node["custom_crons"].find_all { |c| c["instance_name"] == "util" }
 
-    # Find all cron jobs for app master only
-    app_master_crons = node["custom_crons"].find_all { |c| c[:instance_name] == "app_master" }
+  # Find all cron jobs for app master only
+  app_master_crons = node["custom_crons"].find_all { |c| c["instance_name"] == "app_master" }
 
-    # Find all cron jobs for solo only
-    solo_crons = node["custom_crons"].find_all { |c| c[:instance_name] == "solo" }
+  # Find all cron jobs for solo only
+  solo_crons = node["custom_crons"].find_all { |c| c["instance_name"] == "solo" }
 
-    # Find all cron jobs for application instances
-    app_crons = node["custom_crons"].find_all { |c| c[:instance_name] == "app" }
+  # Find all cron jobs for application instances
+  app_crons = node["custom_crons"].find_all { |c| c["instance_name"] == "app" }
 
-    # Find all cron jobs for ALL instances
-    all_crons = node["custom_crons"].find_all { |c| c[:instance_name] == "all" }
+  # Find all cron jobs for ALL instances
+  all_crons = node["custom_crons"].find_all { |c| c["instance_name"] == "all" }
 
-    # Find all cron jobs for Database instances
-    db_crons = node["custom_crons"].find_all { |c| c[:instance_name] == "db" }
+  # Find all cron jobs for Database instances
+  db_crons = node["custom_crons"].find_all { |c| c["instance_name"] == "db" }
 
-    crons = all_crons + named_crons
-    if node["dna"]["instance_role"] == "util"
-      crons += util_crons
-    end
+  crons = all_crons + named_crons
 
-    if node["dna"]["instance_role"] == "app_master"
-      crons += app_master_crons
-    end
-
-    if node["dna"]["instance_role"] == "solo"
-      crons += solo_crons
-    end
-
-    if node["dna"]["instance_role"] == "app" || node["dna"]["instance_role"] == "app_master"
-      crons += app_crons
-    end
-
-    if node["dna"]["instance_role"] == "db_master" || node["dna"]["instance_role"] == "db_slave"
-      crons += db_crons
-    end
-  else
-    crons = []
+  if node["dna"]["instance_role"] == "util"
+    crons = crons + util_crons
   end
-  delete_cron_jobs_not_in_custom(node["owner_name"], crons)
-  delete_cron_jobs_not_in_custom("root", crons)
-  crons.each do |c|
-    custom_cron_name = "custom_cron_#{c['name']}"
-    cron custom_cron_name do
-      user node["owner_name"]
-      action :create
-      minute c[:time].split[0]
-      hour c[:time].split[1]
-      day c[:time].split[2]
-      month c[:time].split[3]
-      weekday c[:time].split[4]
-      command c[:command]
-    end
+
+  if node["dna"]["instance_role"] == "app_master"
+    crons = crons + app_master_crons
+  end
+
+  if node["dna"]["instance_role"] == "solo"
+    crons = crons + solo_crons
+  end
+
+  if node["dna"]["instance_role"] == "app" || node["dna"]["instance_role"] == "app_master"
+    crons = crons + app_crons
+  end
+
+  if node["dna"]["instance_role"] == "db_master" || node["dna"]["instance_role"] == "db_slave"
+    crons = crons + db_crons
+  end
+else
+  crons = []
+end
+
+delete_cron_jobs_not_in_custom(node["owner_name"], crons)
+delete_cron_jobs_not_in_custom("root", crons)
+crons.each do |c|
+  custom_cron_name = "custom_cron_#{c['name']}"
+  cron custom_cron_name do
+    user node["owner_name"]
+    action :create
+    minute c[:time].split[0]
+    hour c[:time].split[1]
+    day c[:time].split[2]
+    month c[:time].split[3]
+    weekday c[:time].split[4]
+    command c[:command]
   end
 end
