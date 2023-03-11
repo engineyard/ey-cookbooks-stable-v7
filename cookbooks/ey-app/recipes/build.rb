75f78562dd550eef1da9297855ff0c345053b401
Rupika <rupika.k@developmentlogics.com>
2023-03-11 11:00:10 +0530

    [FBZ-10544] re-organise ey-mysql recipe

--- cookbooks/ey-app/recipes/build.rb
+++ cookbooks/ey-app/recipes/build.rb
@@ -1,5 +1,6 @@
-include_recipe "ey-db_admin_tools"
+### This is now moved to ey-mysql::default
+#include_recipe "ey-db_admin_tools"
 
-if db_host_is_rds? && node.engineyard.environment[:db_stack_name][/^(mysql\d+|aurora\d+)/]
-  include_recipe "ey-mysql::setup_app_users_dbs"
-end
+#if db_host_is_rds? && node.engineyard.environment[:db_stack_name][/^(mysql\d+|aurora\d+)/]
+#  include_recipe "ey-mysql::setup_app_users_dbs"
+#end
--- cookbooks/ey-init/recipes/main.rb
+++ cookbooks/ey-init/recipes/main.rb
@@ -5,7 +5,7 @@ include_recipe "ey-core"
 include_recipe "ey-custom::before-main"
 include_recipe "ey-base::bootstrap"
 node.engineyard.instance.roles.each { |role| include_recipe "ey-#{role}::prep" }  
-node.engineyard.instance.roles.each { |role| include_recipe "ey-#{role}::build" }
+#node.engineyard.instance.roles.each { |role| include_recipe "ey-#{role}::build" }
 include_recipe "ey-base::post_bootstrap"
 include_recipe "ey-custom::after-main"
 include_recipe "ey-base::custom"
