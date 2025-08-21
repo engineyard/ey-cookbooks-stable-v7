f16f5454c2a7483d9dae2b3a09ee68cce6641c89
tpoland <tyler.poland@gmail.com>
2025-08-21 17:02:58 -0400

    Prevent ey-db-libs cookbook from reverting apt-archive path change in ey-core

--- cookbooks/ey-db-libs/recipes/default.rb
+++ cookbooks/ey-db-libs/recipes/default.rb
@@ -1,7 +1,7 @@
 postgres_version = node["postgresql"]["short_version"].nil? || node["postgresql"]["short_version"] == {} || node["postgresql"]["short_version"].to_i == 11 ? "all" : node["postgresql"]["short_version"]
 
 apt_repository "posgresql" do
-  uri "http://apt.postgresql.org/pub/repos/apt"
+  uri "http://apt-archive.postgresql.org/pub/repos/apt"
   distribution "#{`lsb_release -cs`.strip}-pgdg"
   components ["main"]
   key "https://www.postgresql.org/media/keys/ACCC4CF8.asc"
